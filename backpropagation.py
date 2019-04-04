e9167216cc266a20ad1b8794787b4f7d0b2a3a4b
SergeyChernenko <sergey@chrnk.ru>
2019-04-04 17:46:18 +0300

    backprop

--- backpropagation.py
+++ backpropagation.py
@@ -5,26 +5,30 @@ import cv2 as cv
 (x_train,y_train),(x_test,y_test) = mnist.load_data()
 
 class MNIST:
-    def __init__(self,inputs,outputs):
-        self.w=np.random.normal(0,0.1,(outputs, inputs))
-        
+    def __init__(self,inputs,hidden,outputs):
+        self.w_ih=np.random.normal(0,0.1,(hidden, inputs))
+        self.w_ho=np.random.normal(0,0.1,(outputs, hidden))
+                
     def train(self,inputs_list, outputs_list):
         inputs=np.array(inputs_list,ndmin=2).T
         outputs=np.array(outputs_list,ndmin=2).T
-        out_results=1/(1+np.exp(-(np.dot(self.w,inputs))))
+        hid_results=1/(1+np.exp(-(np.dot(self.w_ih,inputs))))
+        out_results=1/(1+np.exp(-(np.dot(self.w_ho,hid_results))))
         out_errors=(outputs-out_results)
-        delta=self.lr*np.dot(out_errors,inputs.T)
-        self.w+=delta
-        
+        self.w_ho+=self.lr*np.dot(out_errors*out_results*(1.0-out_results),hid_results.T)
+        hid_errors=np.dot(self.w_ho.T,out_errors)
+        self.w_ih+=self.lr*np.dot(hid_errors*hid_results*(1.0-hid_results),inputs.T)
+                
     def test(self,inputs_list):
         inputs=np.array(inputs_list,ndmin=2).T
-        out_results=1/(1+np.exp(-(np.dot(self.w,inputs))))
+        hid_results=1/(1+np.exp(-(np.dot(self.w_ih,inputs))))
+        out_results=1/(1+np.exp(-(np.dot(self.w_ho,hid_results))))
         return out_results
     
     def set_learning_rate(self,rate):
         self.lr=rate
 
-mn = MNIST(784,10)
+mn = MNIST(784,100,10)
 
 def epoch_train(lr):
     mn.set_learning_rate(lr)
@@ -51,5 +55,3 @@ for i in range(epoch):
     lr=lr/1.3
     epoch_train(lr)
     print("Epoch №",i+1,", Accuracy on the test array ",epoch_test())
-    
-
